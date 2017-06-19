6393efbb93512937c89d91bfb20e9f7fb982c71c
Renato Stauffer <renato.stauffer90@googlemail.com>
2017-06-20 01:56:14 +0200

    Revert "Export"
    This reverts commit 1626d68c2eab0de782045ba981a211ef88cdf83b.

--- RSNetworking/RSNetworking/RSNetworking.h
+++ RSNetworking/RSNetworking/RSNetworking.h
@@ -16,6 +16,7 @@ FOUNDATION_EXPORT const unsigned char RSNetworkingVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <RSNetworking/PublicHeader.h>
 #import <RSNetworking/RSHTTPSessionManager.h>
+#import <AFNetworking/AFNetworking.h>
 
 
 
