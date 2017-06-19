d208041378f69b13376237bebf311f7362dcde4d
Renato Stauffer <renato.stauffer90@googlemail.com>
2017-06-20 00:56:18 +0200

    Next testing

--- RSNetworking/RSNetworking/RSHTTPSessionManager.h
+++ RSNetworking/RSNetworking/RSHTTPSessionManager.h
@@ -11,45 +11,5 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface RSHTTPSessionManager : NSObject
-//
-//- (nullable NSURLSessionDataTask *)performGet:(NSString *)URLString
-//                            parameters:(nullable id)parameters
-//                              progress:(nullable void (^)(NSProgress *downloadProgress))downloadProgress
-//                               success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-//                               failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
-//
-//- (nullable NSURLSessionDataTask *)performHead:(NSString *)URLString
-//                             parameters:(nullable id)parameters
-//                                success:(nullable void (^)(NSURLSessionDataTask *task))success
-//                                failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
-//
-//- (nullable NSURLSessionDataTask *)performPost:(NSString *)URLString
-//                             parameters:(nullable id)parameters
-//                               progress:(nullable void (^)(NSProgress *uploadProgress))uploadProgress
-//                                success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-//                                failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
-//
-//- (nullable NSURLSessionDataTask *)performPost:(NSString *)URLString
-//                             parameters:(nullable id)parameters
-//              constructingBodyWithBlock:(nullable void (^)(id <AFMultipartFormData> formData))block
-//                               progress:(nullable void (^)(NSProgress *uploadProgress))uploadProgress
-//                                success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-//                                failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
-//
-//- (nullable NSURLSessionDataTask *)performPut:(NSString *)URLString
-//                            parameters:(nullable id)parameters
-//                               success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-//                               failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
-//
-//- (nullable NSURLSessionDataTask *)performPatch:(NSString *)URLString
-//                              parameters:(nullable id)parameters
-//                                 success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-//                                 failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
-//
-//- (nullable NSURLSessionDataTask *)performDelete:(NSString *)URLString
-//                               parameters:(nullable id)parameters
-//                                  success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-//                                  failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
-
 @end
 NS_ASSUME_NONNULL_END
