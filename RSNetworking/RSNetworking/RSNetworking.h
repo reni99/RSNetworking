0abd4f612e2828a251c0c1b6539efea6ff57b4a4
Renato Stauffer <renato.stauffer90@googlemail.com>
2017-06-19 23:58:54 +0200

    include public headers

--- RSNetworking/RSNetworking/RSNetworking.h
+++ RSNetworking/RSNetworking/RSNetworking.h
@@ -15,5 +15,8 @@ FOUNDATION_EXPORT double RSNetworkingVersionNumber;
 FOUNDATION_EXPORT const unsigned char RSNetworkingVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <RSNetworking/PublicHeader.h>
+#import <RSNetworking.h>
+#import "RSHTTPSessionManager.h"
+
 
 
