36753f429692969d1a7071a694243e55b5a047c5
Renato Stauffer <renato.stauffer90@googlemail.com>
2017-06-20 00:52:21 +0200

    Some testing

--- RSNetworking/RSNetworking/RSHTTPSessionManager.h
+++ RSNetworking/RSNetworking/RSHTTPSessionManager.h
@@ -7,50 +7,49 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "AFNetworking.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface RSHTTPSessionManager : AFHTTPSessionManager
-
-- (nullable NSURLSessionDataTask *)performGet:(NSString *)URLString
-                            parameters:(nullable id)parameters
-                              progress:(nullable void (^)(NSProgress *downloadProgress))downloadProgress
-                               success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-                               failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
-
-- (nullable NSURLSessionDataTask *)performHead:(NSString *)URLString
-                             parameters:(nullable id)parameters
-                                success:(nullable void (^)(NSURLSessionDataTask *task))success
-                                failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
-
-- (nullable NSURLSessionDataTask *)performPost:(NSString *)URLString
-                             parameters:(nullable id)parameters
-                               progress:(nullable void (^)(NSProgress *uploadProgress))uploadProgress
-                                success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-                                failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
-
-- (nullable NSURLSessionDataTask *)performPost:(NSString *)URLString
-                             parameters:(nullable id)parameters
-              constructingBodyWithBlock:(nullable void (^)(id <AFMultipartFormData> formData))block
-                               progress:(nullable void (^)(NSProgress *uploadProgress))uploadProgress
-                                success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-                                failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
-
-- (nullable NSURLSessionDataTask *)performPut:(NSString *)URLString
-                            parameters:(nullable id)parameters
-                               success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-                               failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
-
-- (nullable NSURLSessionDataTask *)performPatch:(NSString *)URLString
-                              parameters:(nullable id)parameters
-                                 success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-                                 failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
-
-- (nullable NSURLSessionDataTask *)performDelete:(NSString *)URLString
-                               parameters:(nullable id)parameters
-                                  success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-                                  failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
+@interface RSHTTPSessionManager : NSObject
+//
+//- (nullable NSURLSessionDataTask *)performGet:(NSString *)URLString
+//                            parameters:(nullable id)parameters
+//                              progress:(nullable void (^)(NSProgress *downloadProgress))downloadProgress
+//                               success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
+//                               failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
+//
+//- (nullable NSURLSessionDataTask *)performHead:(NSString *)URLString
+//                             parameters:(nullable id)parameters
+//                                success:(nullable void (^)(NSURLSessionDataTask *task))success
+//                                failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
+//
+//- (nullable NSURLSessionDataTask *)performPost:(NSString *)URLString
+//                             parameters:(nullable id)parameters
+//                               progress:(nullable void (^)(NSProgress *uploadProgress))uploadProgress
+//                                success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
+//                                failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
+//
+//- (nullable NSURLSessionDataTask *)performPost:(NSString *)URLString
+//                             parameters:(nullable id)parameters
+//              constructingBodyWithBlock:(nullable void (^)(id <AFMultipartFormData> formData))block
+//                               progress:(nullable void (^)(NSProgress *uploadProgress))uploadProgress
+//                                success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
+//                                failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
+//
+//- (nullable NSURLSessionDataTask *)performPut:(NSString *)URLString
+//                            parameters:(nullable id)parameters
+//                               success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
+//                               failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
+//
+//- (nullable NSURLSessionDataTask *)performPatch:(NSString *)URLString
+//                              parameters:(nullable id)parameters
+//                                 success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
+//                                 failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
+//
+//- (nullable NSURLSessionDataTask *)performDelete:(NSString *)URLString
+//                               parameters:(nullable id)parameters
+//                                  success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
+//                                  failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure;
 
 @end
 NS_ASSUME_NONNULL_END
--- RSNetworking/RSNetworking/RSHTTPSessionManager.m
+++ RSNetworking/RSNetworking/RSHTTPSessionManager.m
@@ -10,59 +10,59 @@
 
 @implementation RSHTTPSessionManager
 
-- (nullable NSURLSessionDataTask *)performGet:(NSString *)URLString
-                            parameters:(nullable id)parameters
-                              progress:(nullable void (^)(NSProgress *downloadProgress))downloadProgress
-                               success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-                               failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure {
-    
-    return [self GET:URLString parameters:parameters progress:downloadProgress success:success failure:failure];
-}
-
-- (nullable NSURLSessionDataTask *)performHead:(NSString *)URLString
-                                    parameters:(nullable id)parameters
-                                       success:(nullable void (^)(NSURLSessionDataTask *task))success
-                                       failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure {
-    return [self HEAD:URLString parameters:parameters success:success failure:failure];
-}
-
-- (nullable NSURLSessionDataTask *)performPost:(NSString *)URLString
-                                    parameters:(nullable id)parameters
-                                      progress:(nullable void (^)(NSProgress *uploadProgress))uploadProgress
-                                       success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-                                       failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure {
-    return [self POST:URLString parameters:parameters progress:uploadProgress success:success failure:failure];
-}
-
-- (nullable NSURLSessionDataTask *)performPost:(NSString *)URLString
-                                    parameters:(nullable id)parameters
-                     constructingBodyWithBlock:(nullable void (^)(id <AFMultipartFormData> formData))block
-                                      progress:(nullable void (^)(NSProgress *uploadProgress))uploadProgress
-                                       success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-                                       failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure {
-    return [self POST:URLString parameters:parameters constructingBodyWithBlock:block progress:uploadProgress success:success failure:failure];
-}
-
-- (nullable NSURLSessionDataTask *)performPut:(NSString *)URLString
-                                   parameters:(nullable id)parameters
-                                      success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-                                      failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure {
-    return [self PUT:URLString parameters:parameters success:success failure:failure];
-}
-
-- (nullable NSURLSessionDataTask *)performPatch:(NSString *)URLString
-                                     parameters:(nullable id)parameters
-                                        success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-                                        failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure {
-    return [self PATCH:URLString parameters:parameters success:success failure:failure];
-}
-
-- (nullable NSURLSessionDataTask *)performDelete:(NSString *)URLString
-                                      parameters:(nullable id)parameters
-                                         success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
-                                         failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure {
-    return [self DELETE:URLString parameters:parameters success:success failure:failure];
-}
+//- (nullable NSURLSessionDataTask *)performGet:(NSString *)URLString
+//                            parameters:(nullable id)parameters
+//                              progress:(nullable void (^)(NSProgress *downloadProgress))downloadProgress
+//                               success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
+//                               failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure {
+//    
+//    return [self GET:URLString parameters:parameters progress:downloadProgress success:success failure:failure];
+//}
+//
+//- (nullable NSURLSessionDataTask *)performHead:(NSString *)URLString
+//                                    parameters:(nullable id)parameters
+//                                       success:(nullable void (^)(NSURLSessionDataTask *task))success
+//                                       failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure {
+//    return [self HEAD:URLString parameters:parameters success:success failure:failure];
+//}
+//
+//- (nullable NSURLSessionDataTask *)performPost:(NSString *)URLString
+//                                    parameters:(nullable id)parameters
+//                                      progress:(nullable void (^)(NSProgress *uploadProgress))uploadProgress
+//                                       success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
+//                                       failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure {
+//    return [self POST:URLString parameters:parameters progress:uploadProgress success:success failure:failure];
+//}
+//
+//- (nullable NSURLSessionDataTask *)performPost:(NSString *)URLString
+//                                    parameters:(nullable id)parameters
+//                     constructingBodyWithBlock:(nullable void (^)(id <AFMultipartFormData> formData))block
+//                                      progress:(nullable void (^)(NSProgress *uploadProgress))uploadProgress
+//                                       success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
+//                                       failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure {
+//    return [self POST:URLString parameters:parameters constructingBodyWithBlock:block progress:uploadProgress success:success failure:failure];
+//}
+//
+//- (nullable NSURLSessionDataTask *)performPut:(NSString *)URLString
+//                                   parameters:(nullable id)parameters
+//                                      success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
+//                                      failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure {
+//    return [self PUT:URLString parameters:parameters success:success failure:failure];
+//}
+//
+//- (nullable NSURLSessionDataTask *)performPatch:(NSString *)URLString
+//                                     parameters:(nullable id)parameters
+//                                        success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
+//                                        failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure {
+//    return [self PATCH:URLString parameters:parameters success:success failure:failure];
+//}
+//
+//- (nullable NSURLSessionDataTask *)performDelete:(NSString *)URLString
+//                                      parameters:(nullable id)parameters
+//                                         success:(nullable void (^)(NSURLSessionDataTask *task, id _Nullable responseObject))success
+//                                         failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task, NSError *error))failure {
+//    return [self DELETE:URLString parameters:parameters success:success failure:failure];
+//}
 
 
 
